b6e86b1af26c559f883bffb956d0a5237781c120
willjp93 <willjp93@gmail.com>
2023-03-08 11:03:39 -0500

    mañana

--- Proyect-Test/src/layouts/MainLayout.vue
+++ Proyect-Test/src/layouts/MainLayout.vue
@@ -1,74 +1,54 @@
 <template>
-  <q-layout view="lHh Lpr lFf">
-    <q-header elevated>
+  <q-layout view="hHh Lpr fFr">
+    <q-header reveal elevated class="bg-primary text-white">
       <q-toolbar>
-        <q-btn
-          flat
-          dense
-          round
-          icon="menu"
-          aria-label="Menu"
-          @click="toggleLeftDrawer"
-        />
+        <q-btn dense flat round icon="menu" @click="toggleLeftDrawer" />
 
-        <q-toolbar-title>
-          My App
+        <q-toolbar-title class="col-3">
+          <q-avatar>
+            <img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg" />
+          </q-avatar>
+          Eccomerce William
         </q-toolbar-title>
 
-        <q-space />
-
-        <q-btn-dropdown
-          v-model:menu="isCartNotEmpty"
-          color="primary"
-          label="Cart"
-          icon="shopping_cart"
+        <q-input
+          class="q-pa-xs col-5"
+          v-model="search"
+          rounded
+          dark
+          standout
+          dense
+          type="search"
         >
-          <q-card>
-            <q-card-section>
-              <q-list dense>
-                <q-item
-                  v-for="(item, index) in cartItems"
-                  :key="index"
-                >
-                  <q-item-section>
-                    <q-item-label>{{ item.name }}</q-item-label>
-                    <q-item-label caption>{{ item.price }}</q-item-label>
-                  </q-item-section>
-                  <q-item-section side>
-                    <q-btn
-                      round
-                      dense
-                      flat
-                      icon="remove"
-                      @click="removeItem(index)"
-                    />
-                  </q-item-section>
-                </q-item>
-              </q-list>
-            </q-card-section>
-            <q-card-actions>
-              <q-btn
-                color="primary"
-                label="Go to Cart"
-                @click="goToCart"
-              />
-            </q-card-actions>
-          </q-card>
-        </q-btn-dropdown>
+          <template v-slot:append>
+            <q-icon name="search" />
+          </template>
+        </q-input>
+
+        <div class="col justify-end">
+          <q-btn unelevated dense padding="5px" label="Iniciar sesión " to="login">
+            <q-avatar size="42px">
+              <img src="https://cdn.quasar.dev/img/avatar2.jpg" />
+            </q-avatar>
+          </q-btn>
+
+          <q-btn
+            dense
+            unelevated
+            padding="10px"
+            label="Mi Carrito"
+            icon-right="shopping_cart"
+            @click="toggleRightDrawer"
+          >
+            <q-badge color="red" transparent floating>0</q-badge>
+          </q-btn>
+        </div>
       </q-toolbar>
     </q-header>
 
-    <q-drawer
-      v-model="leftDrawerOpen"
-      show-if-above
-      bordered
-    >
+    <q-drawer show-if-above v-model="leftDrawerOpen" side="left" bordered>
       <q-list>
-        <q-item-label
-          header
-        >
-          Essential Links
-        </q-item-label>
+        <q-item-label header> </q-item-label>
 
         <EssentialLink
           v-for="link in essentialLinks"
@@ -78,78 +58,115 @@
       </q-list>
     </q-drawer>
 
+    <q-drawer v-model="rightDrawerOpen" side="right" overlay bordered>
+      <!-- drawer content -->
+    </q-drawer>
+
     <q-page-container>
       <router-view />
     </q-page-container>
+    <q-footer reveal elevated class="row bg-dark text-white q-pa-xs">
+      <q-toolbar>
+        <q-toolbar-title>
+          <div class="col-2 q-pa-xs">
+            <q-avatar>
+              <img src="~/assets/quasar-logo-vertical.svg" />
+            </q-avatar>
+          </div>
+        </q-toolbar-title>
+        <div class="col-6 q-pa-xs q-gutter-lg" align="center">
+          <q-btn
+            round
+            flat
+            padding="none"
+            icon="lab la-facebook"
+            size="lg"
+            target="_blank"
+            href="https://www.facebook.com/"
+          />
+          <q-btn
+            round
+            flat
+            padding="none"
+            icon="lab la-whatsapp"
+            size="lg"
+            target="_blank"
+            href="https://web.whatsapp.com/"
+          />
+          <q-btn
+            round
+            flat
+            padding="none"
+            icon="lab la-telegram"
+            size="lg"
+            target="_blank"
+            href="https://t.me/Will_l"
+          />
+        </div>
+        <q-separator spaced />
+        <div class="col-3 justify-center text-center">
+          {{ new Date().getFullYear() }} —
+          <strong>Copyright: @willl_l</strong>
+        </div>
+      </q-toolbar>
+    </q-footer>
+
   </q-layout>
 </template>
 
 <script>
-import { defineComponent, ref } from 'vue'
-import EssentialLink from 'components/EssentialLink.vue'
-import { useRouter } from 'vue-router'
+import { ref } from "vue";
+import EssentialLink from "components/EssentialLink.vue";
 
-export default defineComponent({
-  name: 'MainLayout',
-
+export default {
+  name: "MainLayout",
   components: {
-    EssentialLink
+    EssentialLink,
   },
+  setup() {
+    const leftDrawerOpen = ref(false);
+    const rightDrawerOpen = ref(false);
 
-  setup () {
-    const leftDrawerOpen = ref(false)
-    const cartItems = ref([])
-    const router = useRouter()
-
-    function toggleLeftDrawer () {
-      leftDrawerOpen.value = !leftDrawerOpen.value
-    }
-
-    function removeItem (index) {
-      cartItems.value.splice(index, 1)
-    }
-
-    function goToCart () {
-      const itemsParam = cartItems.value.map(item => encodeURIComponent(JSON.stringify(item))).join(',')
-      router.push(`/cart?items=${itemsParam}`)
-    }
-
-    const isCartNotEmpty = computed(() => cartItems.value.length > 0)
+    const essentialLinks = [
+      {
+        title: "Home",
+        caption: "Homepage",
+        icon: "home",
+        link: "/",
+      },
+      {
+        title: "Products",
+        caption: "View our products",
+        icon: "store",
+        link: "/products",
+      },
+      {
+        title: "About Us",
+        caption: "Learn about our company",
+        icon: "info",
+        link: "/about",
+      },
+      {
+        title: "Contact Us",
+        caption: "Get in touch",
+        icon: "email",
+        link: "/contact",
+      },
+    ];
 
     return {
-      essentialLinks: [
-        {
-          title: 'Home',
-          caption: 'Homepage',
-          icon: 'home',
-          link: '/'
-        },
-        {
-          title: 'Products',
-          caption: 'View our products',
-          icon: 'store',
-          link: '/products'
-        },
-        {
-          title: 'About Us',
-          caption: 'Learn about our company',
-          icon: 'info',
-          link: '/about'
-        },
-        {
-          title: 'Contact Us',
-          caption: 'Get in touch',
-          icon: 'email',
-          link: '/contact'
-        }
-      ],
+      search: ref(""),
+      essentialLinks,
       leftDrawerOpen,
-      toggleLeftDrawer,
-      cartItems,
-      removeItem,
-      goToCart,
-      isCartNotEmpty
-    }
-  }
-})
+      toggleLeftDrawer() {
+        leftDrawerOpen.value = !leftDrawerOpen.value;
+      },
+
+      rightDrawerOpen,
+      toggleRightDrawer() {
+        rightDrawerOpen.value = !rightDrawerOpen.value;
+      },
+    };
+  },
+};
 </script>
